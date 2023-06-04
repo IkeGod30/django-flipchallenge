4d69752b96a98f78f1551ae3d533a2b53a8283a5
Tony Izuogu <anthoizu@yahoo.com>
2023-06-03 20:24:04 -1100

    Registered new model in admin.py

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from first_app.models import Register
+from first_app.models import Register, QuesModel
 
 # Register your models here.
 admin.site.register(Register)
+admin.site.register(QuesModel)
